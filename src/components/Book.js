3882ad7ebfee7cfc3b1322c6747b1a3a3348889b
github username <github email address>
2019-08-14 12:38:49 -0400

    Completed 1/7th of the requirementsgit add .!

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,10 +1,18 @@
 import React from "react";
+import Bookshelf from '../containers/Bookshelf'
 
 const Book = props => {
+  console.log('Props from Book Component', props)
+
+  handleClick = (e) => {
+    console.log(e.target);
+    <Bookshelf bookInfo={props} />
+  }
+
   return (
-    <div>
-      <h2>{/*book title*/}</h2>
-      {/*book img*/}
+    <div onClick={handleClick}>
+      <h2>{props.bookInfo.title}</h2>
+      <img src={props.bookInfo.img} alt="book" />
     </div>
   );
 };
--- src/containers/BookList.js
+++ src/containers/BookList.js
@@ -3,12 +3,33 @@ import Book from "../components/Book";
 import Form from "../components/Form";
 
 class BookList extends Component {
+
+  state = {
+    bookList: []
+  };
+
+
+  componentDidMount() {
+    fetch(`http://localhost:3005/books`)
+      .then(resp => resp.json())
+      .then(data => {
+        this.setState(() => ({ bookList: [...data] }))
+      })
+      .catch(err => console.log(err))
+  }
+
+
   render() {
+    console.log(this.state.bookList)
     return (
       <div className="book-list">
         <h1>Book List</h1>
         <Form />
-        <ul>{/*render list of books here*/}</ul>
+        <ul>{
+          this.state.bookList.map(book => {
+            return <Book key={book.id} bookInfo={book} />
+          })
+        }</ul>
       </div>
     );
   }
